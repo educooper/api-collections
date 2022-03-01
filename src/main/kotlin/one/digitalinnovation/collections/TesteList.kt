20e1518fb674c2413fdb8ca8b968e3fe118a690b
educooper <edusardo@uninove.edu.br>
2022-02-28 21:25:13 -0300

    using a list of data with GroupdBy

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main(){
-    val joao = Funcionario("João", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("João", 1000.0, "CLT")
+    val pedro = Funcionario("Pedro", 2000.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
 
@@ -18,11 +18,15 @@ fun main(){
     funcionarios.sortedBy { it.salario }
                 .forEach { println(it) }
     println("---------------------------------------------")
+
+    funcionarios.groupBy { it.tipoContratacao}
+                .forEach { println(it) }
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ) {
     override fun toString(): String =
         """
